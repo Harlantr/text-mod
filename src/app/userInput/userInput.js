cbd3e59a649d5896889af358052f59679acf1c9a
Thomas Harlan <thomasrharlan@gmail.com>
2017-08-06 19:15:37 -0400

    Removed/Restructured a bit

--- src/app/userInput/userInput.js
+++ src/app/userInput/userInput.js
@@ -2,35 +2,13 @@ import React, { Component } from 'react';
 import TextInput from './textInput';
 import CountInput from './countInput';
 
-class UserInput extends Component {
-    constructor(props) {
-        super(props);
-        this.handleTextChange = this.handleTextChange.bind(this);
-        this.handleCountChange = this.handleCountChange.bind(this);
-    }
-
-    handleTextChange(text) {
-        /*
-            Propagate the text change
-        */
-        this.props.textCallback(text);
-    }
-
-    handleCountChange(count) {
-        /*
-            Propagate the count change
-        */
-        this.props.countCallback(count);
-    }
-
-    render() {
-        return (
-            <div>
-                <TextInput textCallback={this.handleTextChange}/>
-                <CountInput countCallback={this.handleCountChange}/>
-            </div>
-        );
-    }
+const UserInput = () => {
+    return (
+        <div>
+            <TextInput />
+            <CountInput />
+        </div>
+    );
 }
 
 export default UserInput
